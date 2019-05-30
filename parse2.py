2f8b74183d961d3ee4ff5f445be1570650480869
Chris Rehn <crehn@outlook.com>
2019-05-30 14:37:48 +0900

    Add minimal flat parser

---
+++ parse2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+import music21
+
+
+def pitch_accidental(pitch):
+    if pitch.accidental:
+        if "sharp" in pitch.accidental.name:
+            return "sharp"
+        if "flat" in pitch.accidental.name:
+            return "flat"
+    return "natural"
+
+
+def process_part(part):
+    for note in part.flat.notes:
+        for pitch in note.pitches:
+            yield {
+                "time": note.offset,
+                "note": pitch.diatonicNoteNum - 1,
+                "duration": note.duration.quarterLength / 4,
+                "accidental": pitch_accidental(pitch),
+            }
+
+
+def parse(filename):
+    for part in music21.converter.parse(filename).parts:
+        yield list(process_part(part))
+
+
+def main():
+    print(json.dumps(list(parse(sys.argv[1]))))
+
+
+if __name__ == "__main__":
+    main()
