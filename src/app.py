96054f854d5ca6fcb23fec2f53c938965118de5b
Chris Rehn <crehn@outlook.com>
2022-10-16 14:16:17 -0700

    Polish the Streamlit app

--- src/app.py
+++ src/app.py
@@ -10,10 +10,9 @@ from render import render
 st.set_page_config(page_title="Pyogi Converter", page_icon=":notes:")
 st.title("Pyogi Converter")
 
+st.markdown("Convert scores into the [Pyogi notation](https://pyogi.org).")
 
-f = st.file_uploader(
-    "File", help="File must be in MusicXML format.", type=["mxl", "musicxml"]
-)
+f = st.file_uploader("File", help="File must be in MusicXML format.", type=["mxl"])
 if f:
     # TODO: Can't get music21 to accept zipped MusicXML bytes as input
     with tempfile.TemporaryDirectory() as dir:
@@ -24,4 +23,8 @@ if f:
             if len(paths) == 1:
                 filename = paths[0]
                 b = render(parse(filename))
+                st.subheader("Result")
                 st.image(b)
+                st.download_button("Download", b, "result.svg")
+
+st.caption("Source code available [here](https://github.com/hoffa/pyogi-notation).")
