e68d21402c9fef364e5b681da43b7a6fc6c33ac8
Chris Rehn <crehn@outlook.com>
2022-12-27 20:44:16 +0100

    Add test with paging

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
+# Test Fur Elise with default paging
+diff -u testdata/output/fur-elise-paged.svg.txt <(.venv/bin/python src/main.py testdata/input/fur-elise.mxl "$(cat testdata/input/fur-elise.txt)")
+
 echo '# Examples' >examples.md
 
+# Test all inputs with a single scroll (huge yratio)
 for f in testdata/input/*.mxl; do
 	expected=testdata/output/$(basename "$f" .mxl).svg
 	title=$(cat "testdata/input/$(basename "$f" .mxl).txt")
