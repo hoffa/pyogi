6e3b98230e08e2f835c85531083e2858533d3db5
Chris Rehn <crehn@outlook.com>
2022-12-27 21:12:13 +0100

    Add PDF export

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import argparse
 from pathlib import Path
 
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF
+
 from parse import parse
 from render import render
 
@@ -11,6 +14,7 @@ def main() -> None:
     parser.add_argument("title")
     parser.add_argument("--yratio", type=float, default=1.414)  # A4
     parser.add_argument("--export", action="store_true")
+    parser.add_argument("--pdf", action="store_true")
     args = parser.parse_args()
 
     subtitle, title = args.title.split("{sub}", 1)
@@ -21,8 +25,14 @@ def main() -> None:
         for i, svg in enumerate(svgs):
             p = str(args.file)
             page_path = Path(p[: p.rindex(".")] + f"_{i + 1}.svg")
+            pdf_page_path = Path(p[: p.rindex(".")] + f"_{i + 1}.pdf")
             print(f"Writing {page_path}")
             page_path.write_text(str(svg))
+
+            if args.pdf:
+                print(f"Writing {pdf_page_path}")
+                drawing = svg2rlg(str(page_path))
+                renderPDF.drawToFile(drawing, str(pdf_page_path))
     else:
         # One line per page
         print("\n".join(str(svg) for svg in svgs))
