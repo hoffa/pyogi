1b551c041dbe6b152fc2e682f8d0c95dddde8f89
Chris Rehn <crehn@outlook.com>
2022-12-27 23:14:35 +0100

    Create single PDF with --pdf

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import argparse
+import os
+import tempfile
 from pathlib import Path
+from typing import List
 
 from PyPDF2 import PdfWriter
 from reportlab.graphics import renderPDF  # type: ignore
@@ -9,40 +12,48 @@ from parse import parse
 from render import render
 
 
+def svg2pdf(svg: Path, pdf: Path) -> None:
+    drawing = svg2rlg(svg)
+    renderPDF.drawToFile(drawing, str(pdf))
+
+
+def merge_pdf(output: Path, pdfs: List[Path]) -> None:
+    merger = PdfWriter()
+    for path in pdfs:
+        merger.append(str(path))
+    merger.write(str(output))
+    merger.close()
+
+
+def replace_suffix(path: Path, suffix: str) -> Path:
+    return Path(os.path.splitext(path)[0] + suffix)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file", type=Path)
     parser.add_argument("title")
     parser.add_argument("--yratio", type=float, default=1.414)  # A4
-    parser.add_argument("--export", action="store_true")
+    parser.add_argument("--pdf", type=Path)
     args = parser.parse_args()
 
     subtitle, title = args.title.split("{sub}", 1)
 
     svgs = render(parse(args.file), title, subtitle, args.yratio)
 
-    if args.export:
-        pdf_paths = []
+    if args.pdf:
+        with tempfile.TemporaryDirectory() as _tmpdir:
+            tmpdir = Path(_tmpdir)
+            pdf_paths = []
 
-        for i, svg in enumerate(svgs):
-            p = str(args.file)
-            page_path = Path(p[: p.rindex(".")] + f"_{i + 1}.svg")
-            pdf_page_path = Path(p[: p.rindex(".")] + f"_{i + 1}.pdf")
-            pdf_paths.append(pdf_page_path)
-            print(f"Writing {page_path}")
-            page_path.write_text(str(svg))
+            for i, svg in enumerate(svgs):
+                page_path = tmpdir / f"tmp_{i}.svg"
+                pdf_page_path = tmpdir / f"tmp_{i}.pdf"
+                pdf_paths.append(pdf_page_path)
+                page_path.write_text(str(svg))
+                svg2pdf(page_path, pdf_page_path)
 
-            print(f"Writing {pdf_page_path}")
-            drawing = svg2rlg(str(page_path))
-            renderPDF.drawToFile(drawing, str(pdf_page_path))
-
-        merged_pdf_path = Path(p[: p.rindex(".")] + ".pdf")
-        merger = PdfWriter()
-        for path in pdf_paths:
-            merger.append(path)
-        print(f"Writing {merged_pdf_path}")
-        merger.write(merged_pdf_path)
-        merger.close()
+            merge_pdf(args.pdf, pdf_paths)
     else:
         # One line per page
         print("\n".join(str(svg) for svg in svgs))
