f395c076ed2a66ba835348f2092997edd82b8d9d
Chris Rehn <crehn@outlook.com>
2022-12-29 15:04:20 +0100

    Fix pitches

--- src/parse.py
+++ src/parse.py
@@ -5,18 +5,10 @@ from typing import Iterator, List, Literal
 import music21
 from music21.stream import Part, Score
 
-# Only including full steps from
 # https://github.com/cuthbertLab/music21/blob/1025c6cc5703e27ad5eb924c0098e11e3dd04f3b/music21/pitch.py#L93-L107
 Accidental = Literal[
     "natural",
     "sharp",
-    "double-sharp",
-    "triple-sharp",
-    "quadruple-sharp",
-    "flat",
-    "double-flat",
-    "triple-flat",
-    "quadruple-flat",
 ]
 
 
@@ -31,15 +23,13 @@ def get_notes(part: Part) -> Iterator[Note]:
     for note in part.flat.notes:
         time = float(note.offset)
         for pitch in note.pitches:
+            pitch.simplifyEnharmonic(inPlace=True)
             note = pitch.diatonicNoteNum - 1
             accidental = pitch.accidental.name if pitch.accidental else "natural"
-            # Convert flats to sharps
-            # TODO: Check correct and handle doubles
-            if "flat" in accidental:
+            if accidental not in ["natural", "sharp", "flat"]:
+                raise Exception("Unsupported accidental: " + accidental)
+            if accidental == "flat":
                 note -= 1
-                accidental = "sharp"
-            # TODO: Handle doubles; this might be wrong
-            if "sharp" in accidental:
                 accidental = "sharp"
             yield Note(
                 time=time,
